5e48e0c48b3dd424014067250a3733a2e100dc1d
Marucha <marucha.webdev@yahoo.com>
2023-11-13 09:13:13 +0300

    feat: Basic server with CORS response headers

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
+	"log"
+	"net/http"
 )
 
+// middleware function that adds CORS headers to the response
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+
 func main(){
-	fmt.Println("I'm a bird, chirp! Chirp!")
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	// server that uses the corsMux as the handler
+	server :=  http.Server{
+		Addr: ":3000",
+		Handler: corsMux,
+	}
+
+	// listen for request on the server
+	if err := server.ListenAndServe(); err != nil {
+		log.Panic(err)
+		return
+	}
 }
