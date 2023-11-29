82743d8bb72bab335c2eb221b6867a13c6ac93d1
Marucha <marucha.webdev@yahoo.com>
2023-11-29 14:38:49 +0300

    feat: Move helpers into individual package;

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/JohnstoneDev/chirpy/internal/handlers"
 	"github.com/JohnstoneDev/chirpy/internal/helpers"
 	"github.com/go-chi/chi/v5"
 )
@@ -42,8 +42,8 @@ func main(){
 	adminRouter.Handle("/metrics", apiCfg.ReportMetrics(adminHandler))
 	adminRouter.Handle("/admin", apiCfg.ReportMetrics(adminRootHandler))
 
-	apiRouter.Get("/healthz", handlerReadiness) 	// Restricted to GET only
-	apiRouter.Post("/validate_chirp", validateChirpHandler)
+	apiRouter.Get("/healthz", handlers.HandlerReadiness) 	// Restricted to GET only
+	apiRouter.Post("/validate_chirp", handlers.ValidateChirpHandler)
 	apiRouter.Handle("/reset", apiCfg.MiddlewareResetInfo(fs))
 
 	// Mount the api router to the /api path
@@ -65,72 +65,7 @@ func main(){
 	}
 }
 
-// readiness Handler : checks if the server is ready to receive requests
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
 
-// validate chirp POST handler
-func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
-	type Received struct {
-		Body string `json:"body"`
-	}
-
-	type ErrorResponse struct {
-		Error string `json:"error"`
-	}
-
-	type validResp struct {
-		Cleaned_body string `json:"cleaned_body"`
-	}
-
-	resp := ErrorResponse{
-		Error: "Something went wrong",
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	parameters := Received{}
-
- 	err := decoder.Decode(&parameters)
-	if err != nil {
-		log.Println("Error decoding parameters:", err)
-		w.WriteHeader(http.StatusBadRequest)
-
-		if data, err  := json.Marshal(resp); err == nil {
-			w.Write(data)
-		} else {
-			log.Println("Error marshaling response:", err)
-		}
-
-		return
-	}
-
-	// parameters is populated successfully
-	if len(parameters.Body) > 140 {
-		w.WriteHeader(http.StatusBadRequest)
-
-		data, _ := json.Marshal(ErrorResponse{
-			Error: "Chirp is too long",
-		})
-
-		w.Write(data)
-
-		return
-	}
-
-	cleanResponse := helpers.ReplaceProfanity(parameters.Body)
-
-	data, _ := json.Marshal(validResp{
-		Cleaned_body: cleanResponse,
-	})
-
-	// the request body to return
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
-}
 
 // middleware function that adds CORS headers to the response
 func middlewareCors(next http.Handler) http.Handler {
